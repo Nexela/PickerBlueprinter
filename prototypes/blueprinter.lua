36b0c7c3b9f2181d115a564bf98a20094132fa3b
Nexela <nexela@gmail.com>
2020-10-11 02:24:41 -0400

    Clean up prototype loading.

--- prototypes/blueprinter.lua
+++ prototypes/blueprinter.lua
@@ -88,14 +88,12 @@ if updater then
         subgroup = 'tool',
         order = 'c[automated-construction]-a[blueprint-updater]-no-picker',
         stack_size = 1,
-        stackable = false,
         selection_color = {r = 0, g = 1, b = 0},
         alt_selection_color = {r = 0, g = 1, b = 0},
         selection_mode = {'blueprint'},
         alt_selection_mode = {'blueprint'},
         selection_cursor_box_type = 'copy',
         alt_selection_cursor_box_type = 'copy',
-        show_in_library = false
     }
 end
 Data {
